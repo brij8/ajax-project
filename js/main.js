97a6d532b706e1051d11efc32aed6ba0121427f2
brij <bmuses@gmail.com>
2022-03-01 17:39:48 -0800

    F1, T2, on click open file explorer

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// event listener on upload button, open file explorer
+var $upload = document.querySelector('.upload');
+$upload.addEventListener('click', getFile);
+
+const fileOptions = {
+  types: [
+    {
+      description: 'Images',
+      accept: {
+        'image/*': ['.png', '.gif', '.jpeg', '.jpg']
+      }
+    }
+  ],
+  excludeAcceptAllOption: true,
+  multiple: false
+};
+
+async function getFile() {
+  // open file picker
+  var [fileHandle] = await window.showOpenFilePicker(fileOptions);
+
+  // get file contents
+  const srcImage = await fileHandle.getFile();
+
+  // assign img element to var
+  var imgRef = document.querySelector('.src-img');
+
+  // make a URL for image file on local dir
+  var imgURL = URL.createObjectURL(srcImage);
+
+  // assign that URL to src of img element
+  imgRef.src = imgURL;
+}
